1bbded26ded34ab78bf8c903391f8659e3a01e3d
Daniil_Shurygin <Daniil_Shurygin@epam.com>
2021-03-29 22:24:05 +0400

    FE: Add ProductHomeIndex view

---
+++ src/views/Products/views/ProductHomeIndex/ProductHomeIndex.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="grey lighten-5">
+    <v-container>
+      <v-row>
+        <v-col
+          xs="12"
+          sm="6"
+          md="4"
+          col="3"
+          v-for="(product, index) in products"
+          :key="product.id"
+        >
+          <v-card class="pa-2" outlined tile>
+            <v-img
+              height="250"
+              :src="`https://source.unsplash.com/random?sig=${index}`"
+            ></v-img>
+
+            <v-card-title>{{ product.title }}</v-card-title>
+            <v-card-text>
+              <p class="my-4 subtitle-1">
+                {{ product.description }}
+              </p>
+              <p>{{ product.price }}</p>
+              <p>{{ product.count }}</p>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-add-product-to-cart
+                :maxCount="product.count"
+              ></v-add-product-to-cart>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import VAddProductToCart from "./ui/AddProductToCart.vue";
+
+import { productApi } from '@/api/product-api';
+
+// TODO: avoid any, model
+export default Vue.extend({
+  name: "ProductHomeIndex",
+  components: { VAddProductToCart },
+  data() {
+    return {
+      isFetching: false,
+      products: [],
+      error: null,
+    };
+  },
+  created() {
+    this.fetchProducts();
+  },
+  methods: {
+    fetchProducts() {
+      this.isFetching = true;
+
+      productApi
+        .fetchAllProducts()
+        .then((products: any) => {
+          this.products = products;
+        })
+        .finally(() => {
+          this.isFetching = false;
+        });
+    },
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ src/views/Products/views/ProductHomeIndex/ui/AddProductToCart.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="productCount < 1">
+    <v-btn color="pink" icon large @click="incrementProduct()">
+      <v-icon>shopping_cart</v-icon>
+    </v-btn>
+  </div>
+  <div v-else>
+    <v-btn icon large @click="decrementProduct()">
+      <v-icon>remove</v-icon>
+    </v-btn>
+
+    <span class="px-3">
+      {{ productCount }}
+    </span>
+
+    <v-btn
+      :disabled="isAddButtonDisabled"
+      icon
+      large
+      @click="incrementProduct()"
+    >
+      <v-icon>add</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "AddProductToCart",
+  props: {
+    maxCount: Number,
+  },
+  data () {
+    return {
+      productCount: 0,
+    };
+  },
+  computed: {
+    isAddButtonDisabled(): boolean {
+      return this.productCount >= this.maxCount;
+    },
+  },
+  methods: {
+    incrementProduct() {
+      this.productCount += 1;
+    },
+    decrementProduct() {
+      this.productCount -= 1;
+    },
+  },
+});
+</script>
