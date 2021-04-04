0a4a88106073e9851c4d1939a6e7568077b9c5e4
Daniil_Shurygin <Daniil_Shurygin@epam.com>
2021-04-04 23:28:34 +0400

    FE: eslint alive! add regex rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,20 @@ module.exports = {
   },
   extends: [
     "plugin:vue/essential",
-    "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier/@typescript-eslint",
+    'plugin:prettier/recommended',
+    "plugin:clean-regex/recommended",
   ],
   parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module', 
     ecmaVersion: 2020,
   },
+  plugins: [
+    "clean-regex"
+  ],
   rules: {
+    "no-return-await": "error",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
--- src/components/Snackbar/SnackbarContainer.vue
+++ src/components/Snackbar/SnackbarContainer.vue
@@ -1,28 +1,28 @@
 <template>
-  <div v-show="isErrorSnackbar">
-    <v-error-snackbar v-model="snackbar.visible" :message="snackbar.message">
-    </v-error-snackbar>
-  </div>
+	<div v-show="isErrorSnackbar">
+		<v-error-snackbar v-model="snackbar.visible" :message="snackbar.message" />
+	</div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
 
-import { SnackbarType } from "@/store/snackbar";
+import { SnackbarType } from '@/store/snackbar';
+import type { SnackbarState } from '@/store/snackbar';
 
-import VErrorSnackbar from "./ui/ErrorSnackbar.vue";
+import VErrorSnackbar from './ui/ErrorSnackbar.vue';
 
 export default Vue.extend({
-  components: { VErrorSnackbar },
-  computed: {
-    snackbar() {
-      return this.$store.state.snackbar;
-    },
-    isErrorSnackbar() {
-      const snackbar = this.$store.state.snackbar;
+	components: { VErrorSnackbar },
+	computed: {
+		snackbar() {
+			return this.$store.state.snackbar;
+		},
+		isErrorSnackbar() {
+			const snackbar: SnackbarState = this.snackbar;
 
-      return snackbar.type === SnackbarType.Error;
-    },
-  },
+			return snackbar.type === SnackbarType.Error;
+		},
+	},
 });
 </script>
