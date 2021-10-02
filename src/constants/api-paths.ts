1d32c1a3007b489bba184ef8fdec3eee23517a1d
Maksim Shylau <shilaums@gmail.com>
2021-10-02 20:24:47 +0300

    feat: update API path

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -2,6 +2,6 @@ export const API_PATHS = {
 	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://1j2lf9z0dj.execute-api.eu-west-1.amazonaws.com/dev',
-	bff: 'https://nkoz58izxd.execute-api.eu-west-1.amazonaws.com/dev',
+	bff: 'https://jr7mz7c9nc.execute-api.eu-west-1.amazonaws.com/dev/',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
