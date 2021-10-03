d8741a97522ff71861da90a3d9fe2cfe4c434de6
Maksim Shylau <shilaums@gmail.com>
2021-10-03 13:05:35 +0300

    feat: update API paths

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
 	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://1j2lf9z0dj.execute-api.eu-west-1.amazonaws.com/dev',
-	bff: 'https://jr7mz7c9nc.execute-api.eu-west-1.amazonaws.com/dev/',
+	import: 'https://hifx2vr6f4.execute-api.eu-west-1.amazonaws.com/dev',
+	bff: 'https://icqa4iybt4.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
