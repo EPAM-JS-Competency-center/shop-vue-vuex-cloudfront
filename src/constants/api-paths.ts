3b72b089c80e8cc1dab8a23380d98039c8307ac0
glebovanton <workglebov@gmail.com>
2021-11-06 05:48:24 +0300

    feat: Fix bff path

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -2,7 +2,7 @@ export const API_PATHS = {
 	product: 'https://0ybyxoymha.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://lizpddj0i8.execute-api.eu-west-1.amazonaws.com/dev',
-	bff: 'https://0ybyxoymha.execute-api.eu-west-1.amazonaws.com/dev',
+	bff: 'http://glebovanton-bff-api-dev.eu-west-1.elasticbeanstalk.com',
 	cart:
 		'http://glebovanton-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
 };
