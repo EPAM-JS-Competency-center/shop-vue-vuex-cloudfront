b319155c781d50a2f3d7903803d149a3d69393c7
Kuchuk Andrey <kreezag@gmail.com>
2022-06-06 12:13:23 +0400

    set regeon to product-service

--- server/product-service/serverless.ts
+++ server/product-service/serverless.ts
@@ -15,6 +15,7 @@ const serverlessConfiguration: AWS = {
 	provider: {
 		name: 'aws',
 		runtime: 'nodejs14.x',
+		region: 'eu-west-1',
 		apiGateway: {
 			minimumCompressionSize: 1024,
 			shouldStartNameWithService: true,
