fdd31383a91e3721dfa65883e0db684b49547781
Kuchuk Andrey <kreezag@gmail.com>
2022-06-11 14:40:47 +0400

    polish getProductsList lambda

--- server/product-service/src/functions/getProductsList/index.ts
+++ server/product-service/src/functions/getProductsList/index.ts
@@ -1,4 +1,3 @@
-import schema from './schema';
 import { handlerPath } from '@libs/handlerResolver';
 
 export default {
@@ -8,11 +7,6 @@ export default {
 			http: {
 				method: 'GET',
 				path: 'products',
-				request: {
-					schemas: {
-						'application/json': schema,
-					},
-				},
 			},
 		},
 	],
