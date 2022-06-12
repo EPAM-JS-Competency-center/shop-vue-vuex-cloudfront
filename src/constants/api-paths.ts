4042c382e50b3eaca9145a2432cf34e8ce5c1684
Kuchuk Andrey <kreezag@gmail.com>
2022-06-12 13:17:37 +0400

    update bff api path

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -2,6 +2,6 @@ export const API_PATHS = {
 	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	bff: 'https://8v7tgis5n5.execute-api.eu-west-1.amazonaws.com',
+	bff: 'https://q0m94gdbsf.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
