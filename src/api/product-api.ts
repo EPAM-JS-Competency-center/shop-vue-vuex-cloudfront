8c13b543fca3cf437845020bc266d6a86a7d31fe
Kuchuk Andrey <kreezag@gmail.com>
2022-06-12 13:28:10 +0400

    update app api

--- src/api/product-api.ts
+++ src/api/product-api.ts
@@ -7,7 +7,7 @@ import { Product } from '@/models/product';
 
 // const fetchAvailableProducts = async (): Promise<Product[]> => {
 // 	return axios
-// 		.get(`${API_PATHS.bff}/products/available`)
+// 		.get(`${API_PATHS.product}/products/available`)
 // 		.then(res => {
 // 			console.log(res);
 //
@@ -22,7 +22,7 @@ import { Product } from '@/models/product';
 
 const fetchProducts = async (): Promise<Product[]> => {
 	return axios
-		.get(`${API_PATHS.bff}/products`)
+		.get(`${API_PATHS.product}/products`)
 		.then(res => JSON.parse(res.data))
 		.catch(e => {
 			console.error(e);
@@ -34,19 +34,19 @@ const fetchProducts = async (): Promise<Product[]> => {
 const fetchProductById = async (id: string) => {
 	console.info(`GET fetchProductById: ${id}`);
 
-	return axios.get(`${API_PATHS.bff}/products/${id}`).then(res => res.data);
+	return axios.get(`${API_PATHS.product}/products/${id}`).then(res => res.data);
 };
 
 const deleteProductById = (id: string) => {
 	console.info(`DELETE deleteProductById: ${id}`);
 
-	return axios.delete(`${API_PATHS.bff}/products/${id}`);
+	return axios.delete(`${API_PATHS.product}/products/${id}`);
 };
 
 const saveProduct = (productToSave: Product) => {
 	console.info(`PUT saveProduct: ${JSON.stringify(productToSave)}`);
 
-	return axios.put(`${API_PATHS.bff}/products`, productToSave);
+	return axios.put(`${API_PATHS.product}/products`, productToSave);
 };
 
 export const productApi = {
--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
-	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	product: 'https://q0m94gdbsf.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	bff: 'https://q0m94gdbsf.execute-api.eu-west-1.amazonaws.com/dev',
+	bff: 'https://rss-school-test-app.s3.eu-west-1.amazonaws.com',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
