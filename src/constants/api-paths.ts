235229685d99a76c8730f14947f7ec100ca37952
Maxim Revyakin <Maxim_Revyakin@epam.com>
2022-07-31 19:18:43 +0400

    Update import route once again

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
 	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://4o3wnboj0k.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://tid765q6hb.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: 'https://vzxop304db.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
