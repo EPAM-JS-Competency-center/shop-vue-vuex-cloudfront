c78de2228d811228a90e893b30297266e23269de
jose luis ceron imbachi <joseluisceron13@gmail.com>
2022-07-31 21:58:22 -0500

    feat: update import in api-paths

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,8 +1,7 @@
 export const API_PATHS = {
 	product: 'https://bunwbokn0b.execute-api.us-east-1.amazonaws.com/dev',
-	// product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://bunwbokn0b.execute-api.us-east-1.amazonaws.com/dev/',
 	bff: 'https://bunwbokn0b.execute-api.us-east-1.amazonaws.com/dev',
 	// bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
