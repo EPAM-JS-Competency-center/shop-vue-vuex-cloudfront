338ab3f407e6cbcb4da238cdf184ad47aaa78f02
Ivan Rudnitski <ivan_rudnitski@epam.com>
2022-10-30 02:40:15 +0100

    Change import path

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
 	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://nt6192yc2j.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://6st2l9vcuj.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: ' https://e07e81v5pe.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
